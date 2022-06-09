c3249d8dc8e902d4199218fe4ba96fa66133729c
Martín Cardona <martincardonademiguel@gmail.com>
2022-06-09 13:24:21 +0200

    añadidos todos los js( faltan moverlos de los html a los js)

---
+++ js/maps.js
@@ -0,0 +1,49 @@
+
+function openSidebar() {
+    console.log('sidebar');
+    document.querySelector('.sidebar').classList.add('open');
+    document.getElementById('close').style.display = 'block';
+    document.getElementById('overflow').style.overflow = 'hidden';
+    const video = document.getElementById("video");
+    video.play();
+
+}
+
+function closeSidebar() {
+    console.log('sidebar');
+    document.querySelector('.sidebar').classList.remove('open');
+    document.getElementById('close').style.display = 'none';
+    document.getElementById('overflow').style.overflow = 'visible';
+    const video = document.getElementById("video");
+    video.pause();
+}
+function openSidebarMenu() {
+  console.log('sidebar');
+  document.querySelector('.sidebar-mainpage').classList.add('open');
+  document.getElementById('hidden-button-cross').style.display ='block';
+  document.getElementById('overflow').style.overflow = 'hidden';
+}
+
+function closeSidebarMenu() {
+  console.log('sidebar');
+  document.querySelector('.sidebar-mainpage').classList.remove('open');
+  document.getElementById('hidden-button-cross').style.display = 'none';
+  document.getElementById('overflow').style.overflow = 'visible';
+}
+function redirect(pagina) {
+    window.location.href = pagina;
+}
+
+/*STICKY NAVBAR*/
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+if ((window.pageYOffset - 10) >= sticky) {
+    navbar.classList.add("sticky")
+} else {
+    navbar.classList.remove("sticky");
+}
+}
